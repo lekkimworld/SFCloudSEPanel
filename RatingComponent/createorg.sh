1b6271d399be948a58d141cbf71d5657300ef272
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2018-06-20 22:06:33 +0200

    Add data to initial import after scratch org creation

--- RatingComponent/createorg.sh
+++ RatingComponent/createorg.sh
@@ -1,9 +1,13 @@
-sfdx force:org:create -a myscratch -f config/project-scratch-def.json --setdefaultusername
-sfdx l18n:user:set --language en_US -u myscratch
+USERNAME=myscratch
 
-ACCESS_TOKEN=`sfdx force:org:display -u myscratch --json | jq -r ".result.accessToken"`
-INSTANCE_URL=`sfdx force:org:display -u myscratch --json | jq -r ".result.instanceUrl"`
+sfdx force:org:create -a $USERNAME -f config/project-scratch-def.json --setdefaultusername
+sfdx l18n:user:set --language en_US -u $USERNAME
 
-sfdx force:org:open -u myscratch --path /lightning/page/home
+ACCESS_TOKEN=`sfdx force:org:display -u $USERNAME --json | jq -r ".result.accessToken"`
+INSTANCE_URL=`sfdx force:org:display -u $USERNAME --json | jq -r ".result.instanceUrl"`
+
+sfdx force:data:tree:import -u $USERNAME -p ./data/Account-Contact-plan.json
+
+sfdx force:org:open -u $USERNAME --path /lightning/page/home
 
 open -a /Applications/Firefox.app "$INSTANCE_URL/secur/frontdoor.jsp?sid=$ACCESS_TOKEN&retURL=/lightning/page/home"
