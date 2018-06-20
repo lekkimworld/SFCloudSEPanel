be781e6e3a16172743ad3bf4a63def710e84739a
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2018-06-20 18:50:27 +0200

    Change script to open org to homepage and launch Firefox as well

--- RatingComponent/createorg.sh
+++ RatingComponent/createorg.sh
@@ -1,3 +1,9 @@
 sfdx force:org:create -a myscratch -f config/project-scratch-def.json
 sfdx l18n:user:set --language en_US -u myscratch
-sfdx force:org:open -u myscratch
+
+ACCESS_TOKEN=`sfdx force:org:display -u myscratch --json | jq -r ".result.accessToken"`
+INSTANCE_URL=`sfdx force:org:display -u myscratch --json | jq -r ".result.instanceUrl"`
+
+sfdx force:org:open -u myscratch --path /lightning/page/home
+
+open -a /Applications/Firefox.app "$INSTANCE_URL/secur/frontdoor.jsp?sid=$ACCESS_TOKEN&retURL=/lightning/page/home"
